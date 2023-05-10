e83dbba652c8c44ff08ad05478a9ecb717c95453
Tsimafey <timtarl59@gmail.com>
2023-05-10 20:53:09 +0300

    End LogIn

---
+++ app/src/main/java/com/example/hotel/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.hotel
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.example.hotel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        lateinit var auth: FirebaseAuth
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signIn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+            binding.userDetails.text = updateData()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.userDetails.text = updateData()
+    }
+
+    private fun updateData(): String{
+        return "Email : ${auth.currentUser?.email}"
+    }
+}
