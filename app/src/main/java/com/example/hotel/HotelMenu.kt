380504759e26120045d846c51d706bd9551f3141
Tsimafey <timtarl59@gmail.com>
2023-05-11 10:52:46 +0300

    added the design of the main pages

---
+++ app/src/main/java/com/example/hotel/HotelMenu.kt
@@ -0,0 +1,16 @@
+package com.example.hotel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class HotelMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_menu)
+    }
+
+
+
+}
